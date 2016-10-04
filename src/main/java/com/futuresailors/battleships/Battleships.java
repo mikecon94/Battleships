0b1afe600366ddbfbd892f8cc4ebea5848a5fef6
Mike Conroy <mike.conroy@capgemini.com>
2016-10-04 16:32:18 +0100

    Update Main class name.

--- src/main/java/com/futuresailors/battleships/Battleships.java
+++ src/main/java/com/futuresailors/battleships/Battleships.java
@@ -2,7 +2,7 @@ package com.futuresailors.battleships;
 
 import com.futuresailors.battleships.controller.BattleShipsController;
 
-public class BattleShips {
+public class Battleships {
 	public static void main(String[] args){
 		BattleShipsController game = new BattleShipsController();
 		game.start();
