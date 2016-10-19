557e1c9abb251f5c31c1c22169301aea21339fe7
Mike Conroy <mike.conroy@capgemini.com>
2016-10-19 23:10:16 +0100

    Fix Tile positioning creation.

--- src/main/java/com/futuresailors/battleships/model/Ship.java
+++ src/main/java/com/futuresailors/battleships/model/Ship.java
@@ -12,18 +12,18 @@ public class Ship {
 	//Tiles the ship occupys
 	private Tile tiles[];
 	//Image path
-	private String imageLoc;
+	private String imagePath;
 	
 	//Constructor
-	public Ship(int width, int height, String imageLoc){
+	public Ship(int width, int height, String imagePath){
 		this.width = width;
 		this.height = height;
-		this.imageLoc = imageLoc;
+		this.imagePath = imagePath;
 		//Init tile array with the amount of tiles the ship occupys
 		tiles = new Tile[height*width];
 		System.out.println("New Ship Created, Height: " + height + " Width: " + width + " Tiles: " + tiles.length);
-		
-	};
+	}
+	
 	//Used to register a hit
 	public boolean hit(Point location) {
 		for(Tile tile : tiles){
@@ -34,6 +34,7 @@ public class Ship {
 		}
 		return false;
 	}
+	
 	//Checks if any of the tiles have been hit and if they have all been hit returns true
 	public boolean checkIfSunk(){
 		//Loops through each tile
@@ -47,39 +48,48 @@ public class Ship {
 		sink();
 		return true;
 	}
+	
 	//Allows the ship to be marked sunk
 	public void sink(){
 		sunk = true;
 	}
+	
 	//Returns true if the ship is sunk
 	public boolean isSunk() {
 		return sunk;
 	}
+	
 	//Initialises the tiles array based on the top left hand tile
 	public void createTiles(Point pos){
-		System.out.println("Tiles Length: " + tiles.length);
+		System.out.println("Number of Tiles: " + tiles.length);	
+
+		int index = 0;
 		for(int i=0; i < height; i++){
 			System.out.println("Height Loop " + i);
 			for(int j=0; j < width;j++) {
-				System.out.println("Entering Width Loop");
 				Point point = new Point(pos.x+ j,pos.y +i);
-				tiles[i+j] = new Tile(point);
+				tiles[index] = new Tile(point);
 				System.out.println("Width Loop " + j);
+				index++;
 			}
 		}
 	}
+	
 	//Returns width for use in the view
 	public int getWidth(){
 		return width;
 	}
+	
 	//Returns width for use in the view
 	public int getHeight(){
 		return height;
 	}
+	
 	//Returns Image Location
-	public String getImageLoc() {
-		return imageLoc;
+	public String getImagePath() {
+		return imagePath;
 	}
+	
 	//Returns the tiles array
 	public Tile[] getTiles(){
 		return tiles;	
