56dbb84b54f57ad3b88db6f6b0cfa1ad388814e1
Joe Baldwin <joe.baldwin@capgemini.com>
2016-10-21 22:15:43 +0000

    Added comments and return to menu stub to single player controller. MainPlayListener fleshed out but has issues when called will fix.

--- src/main/java/com/futuresailors/battleships/controller/SinglePlayerController.java
+++ src/main/java/com/futuresailors/battleships/controller/SinglePlayerController.java
@@ -1,10 +1,12 @@
 package com.futuresailors.battleships.controller;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import com.futuresailors.battleships.UIHelper;
 import com.futuresailors.battleships.model.Grid;
 import com.futuresailors.battleships.model.Ship;
+import com.futuresailors.battleships.view.MainPlayListener;
 import com.futuresailors.battleships.view.MainPlayPanel;
 
 public class SinglePlayerController {
@@ -16,6 +18,8 @@ public class SinglePlayerController {
 	private Grid grid = new Grid(10);
 	
 	public SinglePlayerController(Ship ships[], JFrame window){
+		//Something wrong with the listener
+		//MainPlayListener listener = new MainPlayListener(panel, this);
 		this.ships = ships;
 		this.window = window;
 		addPanel();
@@ -31,6 +35,10 @@ public class SinglePlayerController {
 	
 	private void placeAIShips(){
 		//This method should generate the AI's ships and randomly place them on the grid
+	}
+
+	public void returnToMenu() {
+		//We should do some sort of check here to where a popup window prompts the player to ask if they are sure they want to leave the game
 	}
 
 }
