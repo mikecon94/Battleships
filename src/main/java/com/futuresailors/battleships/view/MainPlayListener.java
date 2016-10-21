b442293cd2ee57ce9be3af303cde648d1c7ccc69
Joe Baldwin <joe.baldwin@capgemini.com>
2016-10-21 22:16:02 +0000

    MainPlayListener

--- src/main/java/com/futuresailors/battleships/view/MainPlayListener.java
+++ src/main/java/com/futuresailors/battleships/view/MainPlayListener.java
@@ -1,10 +1,44 @@
 package com.futuresailors.battleships.view;
 
-public class MainPlayListener {
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
+import com.futuresailors.battleships.controller.SinglePlayerController;
+
+public class MainPlayListener implements ActionListener{
+
+	private SinglePlayerController controller;
+	private JPanel panel;
+	
+	public MainPlayListener(JPanel panel, SinglePlayerController controller){
+		this.controller = controller;
+		this.panel = panel;
+		addListeners();
 	}
 
+	/**
+	 * Loops round all the components on the main menu panel
+	 * and adds this listener to all of the buttons.
+	 */
+	private void addListeners(){
+		Component[] comps = panel.getComponents();
+		for(Component comp : comps){
+			if(comp instanceof JButton){
+				JButton button = (JButton) comp;
+				button.addActionListener(this);
+			}
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if("Return".equals(e.getActionCommand())){
+			System.out.println("Return Clicked.");
+			controller.returnToMenu();
+		}
+	}
 }
