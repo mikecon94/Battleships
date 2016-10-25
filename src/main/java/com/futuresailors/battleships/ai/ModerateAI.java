987e82a47b1bffc836e7b707a0e4ed8afd115aaa
Mike Conroy <mike.conroy@capgemini.com>
2016-10-25 16:15:12 +0100

    Begin implementation of Moderate AI.

--- src/main/java/com/futuresailors/battleships/ai/ModerateAI.java
+++ src/main/java/com/futuresailors/battleships/ai/ModerateAI.java
@@ -16,33 +16,44 @@ import com.futuresailors.battleships.model.Ship;
  */
 public class ModerateAI implements AI {
 	
-	private Grid grid;
+	private Grid myGrid;
+	private Grid oppGrid;
 	private Ship[] ships;
 	private Point lastGuess = new Point(-1,-1);
 	private boolean flag;
 	
-	public ModerateAI(Grid grid, Ship[] ships){
-		this.grid = grid;
+	public ModerateAI(Grid myGrid, Grid oppGrid, Ship[] ships){
+		this.myGrid = myGrid;
+		this.oppGrid = oppGrid;
 		this.ships = ships;
 	}
 
 	@Override
 	public void placeShips() {
-
+		Point pos = new Point(0,0);
+		for(Ship ship : ships){
+			do {
+				pos.x = ThreadLocalRandom.current().nextInt(0, myGrid.getColumns());
+				pos.y = ThreadLocalRandom.current().nextInt(0, myGrid.getRows());
+			} while (myGrid.checkValidPlace(pos, ship) == false);
+			ship.placeShip(pos);
+			myGrid.placeShip(pos, ship);
+		}
 	}
 
 	@Override
 	public Point takeMove() {
+		Point target = checkForHits();
 		//Generate Guess
-		int x = ThreadLocalRandom.current().nextInt(0, grid.getColumns());
-		int y = ThreadLocalRandom.current().nextInt(0, grid.getRows());
+		int x = ThreadLocalRandom.current().nextInt(0, myGrid.getColumns());
+		int y = ThreadLocalRandom.current().nextInt(0, myGrid.getRows());
 		//Check if this is the first guess
 		if(lastGuess.x == -1 && lastGuess.y == -1){
 			lastGuess.x = x;
 			lastGuess.y = y;
 			return new Point(x,y);
 		}else{//If not the first guess, check if the last guess was a hit
-			if(grid.getTile(lastGuess) == GridTile.HIT){
+			if(myGrid.getTile(lastGuess) == GridTile.HIT){
 				//Hit tile to the left after checking if that is a valid move
 				//Set flag to identify a guess by the strategy has already been made?
 				if(flag == true){
@@ -59,7 +70,14 @@ public class ModerateAI implements AI {
 		return new Point(1,1);
 	}
 	
-	//We should maybe have a method here called public Point calculateNextMove(Boolean wasHit, Point lastMove)
-	//This should, based on whether the last move was a hit decide whether to proceed with a strategy
-	//like hitting adjecent tiles or it should make another random guess.
+	private Point checkForHits(){
+		for(int y = 0; y < oppGrid.getRows(); y++){
+			for(int x = 0; x < oppGrid.getColumns(); x++){
+				if(oppGrid.getTile(new Point(x, y)) == GridTile.HIT){
+					return new Point(x, y);
+				}
+			}
+		}
+		return new Point(-1, -1);
+	}
 }
