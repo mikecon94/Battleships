229e39184cf9fc2ccfa2f7edcab2b2e54238ca1d
Joe Baldwin <joe.baldwin@capgemini.com>
2016-11-09 22:27:12 +0000

    Difficulty selection now works.

---
+++ src/main/java/com/futuresailors/battleships/view/DifficultySelectionPanelListener.java
@@ -0,0 +1,56 @@
+package com.futuresailors.battleships.view;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import com.futuresailors.battleships.controller.GameTypeMenuController;
+import com.futuresailors.battleships.controller.SinglePlayerController;
+
+public class DifficultySelectionPanelListener implements ActionListener{
+	
+	private SinglePlayerController controller;
+	private JPanel panel;
+	
+	public DifficultySelectionPanelListener(JPanel panel, SinglePlayerController controller){
+		this.controller = controller;
+		this.panel = panel;
+		addListeners();
+	}
+	
+	/**
+	 * Loops round all the components on the main menu panel
+	 * and adds this listener to all of the buttons.
+	 */
+	private void addListeners(){
+		Component[] comps = panel.getComponents();
+		for(Component comp : comps){
+			if(comp instanceof JButton){
+				JButton button = (JButton) comp;
+				button.addActionListener(this);
+			}
+		}
+	}
+
+	/**
+	 * Detects which button has been clicked and performs the appropriate
+	 * action.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if("Easy".equals(e.getActionCommand())){
+			System.out.println("Start Game Clicked.");
+			controller.selectEasyMode();
+		} else if("Moderate".equals(e.getActionCommand())){
+			System.out.println("Reloaded Mode Clicked");
+			controller.selectModerateMode();
+		} else if("Hard".equals(e.getActionCommand())){
+			System.out.println("Reloaded Mode Clicked");
+			controller.selectHardMode();
+		}
+	}
+
+}
