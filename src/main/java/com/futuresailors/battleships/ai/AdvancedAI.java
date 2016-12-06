87d0cac8730a3263794838e1b888f37e65c4d661
mikecon <mikecon@outlook.com>
2016-12-06 18:04:40 +0000

    Hard AI now only chooses odd or even squares to hit (randomly chosen at runtime) and when randomly firing will only hit them - therefore not hitting adjacent squares.

--- src/main/java/com/futuresailors/battleships/ai/AdvancedAI.java
+++ src/main/java/com/futuresailors/battleships/ai/AdvancedAI.java
@@ -21,6 +21,7 @@ public class AdvancedAI implements AI {
     private Grid myGrid;
     private Grid oppGrid;
     private Ship[] ships;
+    private final boolean targetEvenSquares;
 
     /**
      * @param myGrid - The AIs grid where it will place it's ships.
@@ -31,6 +32,7 @@ public class AdvancedAI implements AI {
         this.myGrid = myGrid;
         this.oppGrid = oppGrid;
         this.ships = ships;
+        targetEvenSquares = (ThreadLocalRandom.current().nextInt(0, 2) == 1) ? true : false;
     }
 
     public void placeShips() {
@@ -109,17 +111,123 @@ public class AdvancedAI implements AI {
     }
 
     public Point takeMove() {
-        Point target = new Point(0, 0);
-        target.x = ThreadLocalRandom.current().nextInt(0, myGrid.getColumns());
-        target.y = ThreadLocalRandom.current().nextInt(0, myGrid.getRows());
-        // for (int y = 0; y < oppGrid.getColumns(); y++) {
-        // for (int x = 0; x < oppGrid.getRows(); x++) {
-        // if (oppGrid.getTile(new Point(x, y)) == GridTile.SHIP) {
-        // target = new Point(x, y);
-        // return target;
-        // }
-        // }
-        // }
+        Point target = checkForTarget();
+        if (target.x == -1) {
+            // Since there are no targets we choose randomly where to bomb
+            // Like the Simple AI.
+            do {
+                int x = 0;
+                int y = 0;
+                if (targetEvenSquares) {
+                    do {
+                        x = ThreadLocalRandom.current().nextInt(0, oppGrid.getColumns());
+                        y = ThreadLocalRandom.current().nextInt(0, oppGrid.getRows());
+                    } while ((x + y) % 2 != 0);
+                } else {
+                    do {
+                        x = ThreadLocalRandom.current().nextInt(0, oppGrid.getColumns());
+                        y = ThreadLocalRandom.current().nextInt(0, oppGrid.getRows());
+                    } while ((x + y) % 2 == 0);
+                }
+                target = new Point(x, y);
+            } while (oppGrid.getTile(target) == GridTile.MISS
+                    || oppGrid.getTile(target) == GridTile.HIT);
+            return target;
+        } else {
+            return target;
+        }
+    }
+
+    /**
+     * Loops round all the tiles in the opponents grid and if it finds one that has been hit. It
+     * will then check the surrounding tiles and if any are empty it will throw a bomb at them.
+     * 
+     * @return Point - The target to throw the bomb. If it is -1, -1 then there is no good target.
+     */
+    private Point checkForTarget() {
+        for (int y = 0; y < oppGrid.getRows(); y++) {
+            for (int x = 0; x < oppGrid.getColumns(); x++) {
+                Point checkHit = new Point(x, y);
+                // If the tile is hit then we check its surroundings.
+                if (oppGrid.getTile(checkHit) == GridTile.HIT) {
+                    // Get a surrounding valid tile from around the HIT tile.
+                    Point target = getTargetFromHit(checkHit);
+                    // If this isn't -1 then a valid tile has been found.
+                    if (target.x != -1) {
+                        return target;
+                    }
+                }
+            }
+        }
+        return new Point(-1, -1);
+    }
+
+    /**
+     * Checks the surrounding tiles of a given tile and returns any valid tiles to target.
+     * 
+     * @param centreTile - The tile to check the surroundings of.
+     * @return The Point to hit. -1, -1 if no valid tile.
+     */
+    private Point getTargetFromHit(Point centreTile) {
+        // TODO Investigate bug in this code that means sometimes a tile next to a hit tile that is
+        // not hit or miss won't get shot at.
+        // 0 - UP, 1 - RIGHT, 2 - DOWN, 3 - LEFT
+        // Choose a random direction to first check.
+        int startDirection = ThreadLocalRandom.current().nextInt(0, 4);
+        int direction = startDirection;
+        do {
+            // Get the tile in the chosen direction.
+            Point potentialTarget = chooseDirection(direction, centreTile);
+            // Check if it is a valid target (ie. on the grid).
+            if (potentialTarget.x >= 0 && potentialTarget.x < oppGrid.getColumns()
+                    && potentialTarget.y >= 0 && potentialTarget.y < oppGrid.getRows()) {
+                // Check the tile hasn't already been bombed.
+                if (oppGrid.getTile(potentialTarget) != GridTile.HIT
+                        && oppGrid.getTile(potentialTarget) != GridTile.MISS) {
+                    // Return this tile as it is next to a tile that has been hit.
+                    return potentialTarget;
+                }
+            }
+            // Move the direction clockwise.
+            direction++;
+            if (direction == 4) {
+                // Reset the direction once it's gone round.
+                direction = 0;
+            }
+            // When we get back to start direction we have checked all the surrounding tiles.
+        } while (direction != startDirection);
+
+        return new Point(-1, -1);
+    }
+
+    /**
+     * Returns a tile to hit based on the centre tile and direction given.
+     * 
+     * @param direction - An int representing the direction. (0 - UP, 1 - RIGHT, 2 - DOWN, 3 -
+     *            LEFT).
+     * @param centre - A Point object representing the tile to move from.
+     * @return The Point to target
+     */
+    private Point chooseDirection(int direction, Point centre) {
+        Point target = new Point(-1, -1);
+        switch (direction) {
+            // 0 - UP
+            case 0:
+                target.setLocation(centre.x, centre.y - 1);
+                break;
+            // 1 - RIGHT
+            case 1:
+                target.setLocation(centre.x + 1, centre.y);
+                break;
+            // 2 - DOWN
+            case 2:
+                target.setLocation(centre.x, centre.y + 1);
+                break;
+            // 3 - LEFT
+            case 3:
+                target.setLocation(centre.x - 1, centre.y);
+                break;
+        }
         return target;
     }
 }
