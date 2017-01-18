5f407d9ed3712cc3a35ee2f8162c0d45caf05a39
Joe Baldwin <joe.baldwin@capgemini.com>
2017-01-18 20:53:45 +0000

    Multiplayer UI Fix

--- src/main/java/com/futuresailors/battleships/view/multiplayer/HostClientPanel.java
+++ src/main/java/com/futuresailors/battleships/view/multiplayer/HostClientPanel.java
@@ -32,32 +32,17 @@ public class HostClientPanel extends JPanel {
         backBut.setSize(100, 50);
         backBut.setLocation(10, 10);
         backBut.setLayout(null);
-
-        JButton serverBut = new JButton("Start Server");
-        serverBut.setSize(150, 75);
-        serverBut.setLocation((WIDTH / 2) - 75, (int) (HEIGHT - (HEIGHT * 0.75)));
-        serverBut.setLayout(null);
-
-        JButton connectBut = new JButton("Connect");
-        connectBut.setSize(150, 75);
-        connectBut.setLocation((WIDTH / 2) - 75, (int) (HEIGHT - (HEIGHT * 0.40)));
-        connectBut.setLayout(null);
-
         add(backBut);
-        add(serverBut);
-        add(connectBut);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         ImageIcon gridImage = UIHelper.resizeImage("/images/background.jpg", WIDTH, HEIGHT);
         g.drawImage(gridImage.getImage(), 0, 0, this);
-        ;
         g.setFont(new Font("Garamond", Font.BOLD, 50));
         g.setColor(new Color(255, 17, 0));
         g.drawChars("Multiplayer".toCharArray(), 0, 11, (WIDTH / 2) - 120, 50);
+        g.drawChars("Awaiting A Connection".toCharArray(), 0, 21, (WIDTH / 2) - 230, 300);
 
-        g.setFont(new Font("Garamond", Font.PLAIN, 30));
-        g.drawChars("Or connect to a server:".toCharArray(), 0, 23, (WIDTH / 2) - 145, HEIGHT / 2);
     }
 }
