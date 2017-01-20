f1f934bfdd9b0d7a7498eb84568965c025c1a0eb
Ryan Lowers <Ryan Lowers>
2017-01-20 16:52:14 +0000

    added comments and additional javadocs to AI class

--- src/main/java/com/futuresailors/battleships/ai/SimpleAI.java
+++ src/main/java/com/futuresailors/battleships/ai/SimpleAI.java
@@ -25,27 +25,40 @@ public class SimpleAI implements AI {
         this.ships = ships;
     }
     
+    /**
+     * Determines the coordinates of where ships will be placed by generating random numbers
+     * and provided they are in a suitable position then they placed on the board.
+     */
+    
     public void placeShips() {
         Point pos = new Point(0, 0);
         for (Ship ship : ships) {
             do {
+                //X and Y coordinates for placing the ship are generated at random
                 pos.x = ThreadLocalRandom.current().nextInt(0, myGrid.getColumns());
                 pos.y = ThreadLocalRandom.current().nextInt(0, myGrid.getRows());
+                //rotates the ship if the number generated is equal to 1
                 if (ThreadLocalRandom.current().nextInt(0, 2) == 1) {
                     ship.rotateShip();
                 }
+            //If the the ship is in a valid position then it is placed    
             } while (!myGrid.checkValidPlace(pos, ship));
             ship.placeShip(pos);
             myGrid.placeShip(pos, ship);
         }
     }
 
+    /**
+     * Uses a random number to generate coordinates then attacks that point on the grid.
+     */
     public Point takeMove() {
         Point target;
+        // Random number is generated for X and Y coordinates. They are used to instantiate a target
         do {
             int x = ThreadLocalRandom.current().nextInt(0, oppGrid.getColumns());
             int y = ThreadLocalRandom.current().nextInt(0, oppGrid.getRows());
             target = new Point(x, y);
+            //Process is repeated for as long as the target is either a hit, miss or land tile
         } while (oppGrid.getTile(target) == GridTile.MISS
                 || oppGrid.getTile(target) == GridTile.HIT
                 || oppGrid.getTile(target) == GridTile.LAND);
