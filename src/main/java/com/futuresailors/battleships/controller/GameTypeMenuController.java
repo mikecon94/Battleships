b878512d40390f5efab17c15c07f50d658f74920
Ryan Lowers <Ryan Lowers>
2017-01-20 18:33:52 +0000

    Further comments added to game type controller class

--- src/main/java/com/futuresailors/battleships/controller/GameTypeMenuController.java
+++ src/main/java/com/futuresailors/battleships/controller/GameTypeMenuController.java
@@ -15,16 +15,27 @@ import javax.swing.JPanel;
 public class GameTypeMenuController {
 
     private JFrame window;
-
+    
+    /**
+     * Instantiates the JFrame window and then starts it.
+     * @param window this contains the jpanels which displays the menu
+     */
     public GameTypeMenuController(JFrame window) {
         this.window = window;
         start();
     }
 
+    /**
+     * Executes the showMenu method.
+     */
     public void start() {
         showMenu();
     }
-
+    
+    /**
+     * Populates the window JFrame with JPanels and displays the menu by setting them to 
+     * visible.
+     */
     private void showMenu() {
         window.getContentPane().removeAll();
         JPanel gameTypePanel = new GameTypeMenuPanel(UIHelper.getWidth(), UIHelper.getHeight());
@@ -35,16 +46,28 @@ public class GameTypeMenuController {
         window.repaint();
     }
 
+    /**
+     * This method starts Reloaded Mode. The controller is instantiated and then the panel is 
+     * created to show the menu.
+     */
     public void startReloadedMode() {
         @SuppressWarnings("unused")
         ReloadedMapSelectionController controller = new ReloadedMapSelectionController(window);
     }
 
+    /**
+     * This method starts Classic Mode. The controller is instantiated and then the panel is 
+     * created to show the menu.
+     */
     public void startClassicMode() {
         @SuppressWarnings("unused")
         GameTypeController controller = new SinglePlayerController(window);
     }
     
+    /**
+     * Returns to the main menu. The relevant controller is instantiated and then
+     * the main menu is displayed.
+     */
     public void returnToMenu() {
         MainMenuController main = new MainMenuController(window);
         main.showMenu();
